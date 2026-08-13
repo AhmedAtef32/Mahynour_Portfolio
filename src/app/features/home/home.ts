2a306b3d1bbdab4bd104bb73bbd6143b3f705a9a
Ahmed Atef <Ahmedatef32003@gmail.com>
2026-08-13 13:53:13 +0300

    Finish Projects Sections

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroSection } from "./components/hero-section/hero-section";
+import { ProjectsSection } from "./components/projects-section/projects-section";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection],
+  imports: [HeroSection, ProjectsSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
