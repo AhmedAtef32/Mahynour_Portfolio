dc603f6a5d42144c5b795128e188b2ed4e79c634
Ahmed Atef <Ahmedatef32003@gmail.com>
2026-08-14 09:19:16 +0300

    finish WHY THIS MATTERS Section

---
+++ src/app/features/home/components/why-this-matters/why-this-matters.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-this-matters',
+  imports: [],
+  templateUrl: './why-this-matters.html',
+  styleUrl: './why-this-matters.css',
+})
+export class WhyThisMatters {}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroSection } from "./components/hero-section/hero-section";
 import { ProjectsSection } from "./components/projects-section/projects-section";
 import { HowIThink } from "./components/how-ithink/how-ithink";
+import { WhyThisMatters } from "./components/why-this-matters/why-this-matters";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, ProjectsSection, HowIThink],
+  imports: [HeroSection, ProjectsSection, HowIThink, WhyThisMatters],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
