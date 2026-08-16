1baadb6a078b6a1255253bd69f2817f2db34461b
Ahmed Atef <Ahmedatef32003@gmail.com>
2026-08-16 09:17:52 +0300

    fix: Adjust positioning of business goals section and improve code structure in home component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,34 +1,47 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HeroSection } from "./components/hero-section/hero-section";
-import { ProjectsSection } from "./components/projects-section/projects-section";
-import { HowIThink } from "./components/how-ithink/how-ithink";
-import { WhyThisMatters } from "./components/why-this-matters/why-this-matters";
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { HeroSection } from './components/hero-section/hero-section';
+import { ProjectsSection } from './components/projects-section/projects-section';
+import { HowIThink } from './components/how-ithink/how-ithink';
+import { WhyThisMatters } from './components/why-this-matters/why-this-matters';
 import { DesignNote } from './components/design-note/design-note';
-import { BeyondThePixels } from "./components/beyond-the-pixels/beyond-the-pixels";
-import { BehindThePixels } from "./components/behind-the-pixels/behind-the-pixels";
-import { LetsTalk } from "./components/lets-talk/lets-talk";
+import { BeyondThePixels } from './components/beyond-the-pixels/beyond-the-pixels';
+import { BehindThePixels } from './components/behind-the-pixels/behind-the-pixels';
+import { LetsTalk } from './components/lets-talk/lets-talk';
 import { ActivatedRoute } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, ProjectsSection, HowIThink, WhyThisMatters, DesignNote, BeyondThePixels, BehindThePixels, LetsTalk],
+  imports: [
+    HeroSection,
+    ProjectsSection,
+    HowIThink,
+    WhyThisMatters,
+    DesignNote,
+    BeyondThePixels,
+    BehindThePixels,
+    LetsTalk,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit{
-  private  _activatedRoute = inject(ActivatedRoute)
+export class Home implements OnInit {
+  private _activatedRoute = inject(ActivatedRoute);
+  private _platformId = inject(PLATFORM_ID);
 
   ngOnInit(): void {
-    this._activatedRoute.fragment.subscribe({
-      next:(res)=>{
-        console.log(res)
+    if (isPlatformBrowser(this._platformId)) {
+      this._activatedRoute.fragment.subscribe({
+        next: (res) => {
+          console.log(res);
 
-        this.jumpToSection(res!);
-      }
-    })
+          this.jumpToSection(res!);
+        },
+      });
+    }
   }
 
-  jumpToSection(id:string){
-    document.getElementById(id)?.scrollIntoView({behavior:"smooth",block:'start'})
+  jumpToSection(id: string) {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
